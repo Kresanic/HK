ddf7510ec84e0f7846eed2b614d5abb632fd60a4
Peter Kresanic <personal@kresanic.sk>
2023-12-01 10:30:20 +0100

    Initial Commit

---
+++ HK/HKApp.swift
@@ -0,0 +1,17 @@
+//
+//  HKApp.swift
+//  HK
+//
+//  Created by Peter Kresanič on 01/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct HKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
